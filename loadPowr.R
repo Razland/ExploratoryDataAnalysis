bb0c1268e21906a80d33598df30fbb00e5d75a64
dave <anon@dontspam.me>
2014-11-05 20:19:00 -0700

    add code for plot 1

--- loadPowr.R
+++ loadPowr.R
@@ -12,4 +12,12 @@ if(! file.exists("household_power_consumption.txt")) {
 
 PowDat <- read.table( "household_power_consumption.txt", header=T, na.strings="?", sep=";")
 PowDat$Date <- as.Date(as.Date(PowDat$Date, format="%d/%m/%Y"))
-PowDat <- filter( PowDat, PowDat$Date == as.Date("2007-02-01") | PowDat$Date == as.Date("2007-02-02"))
+PowDat <- filter( PowDat, PowDat$Date == as.Date("2007-02-01") | PowDat$Date == as.Date("2007-02-02"))j
+
+#plot 1
+
+par(pin = c(4, 4))
+hist(PowDat$Global_active_power, col = "red", freq = TRUE, main = " ", xlab = "Global Active Power (kilowatts)")
+
+dev.copy(png, file = "plot1.png")
+dev.off()
