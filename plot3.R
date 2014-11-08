d1e69fdd27bf79c08b26438f8b6728d34841e503
dave <anon@dontspam.me>
2014-11-08 14:31:12 -0700

    mostly done functions.  Still need to modify plot4.R axis labels.

--- plot3.R
+++ plot3.R
@@ -14,6 +14,8 @@ if(file.exists("plot3.png")){
 
   x_start <- strptime("2007-02-02 10:00:00", format="%Y-%m-%d %H:%M:%S")
   x_end <- strptime("2007-02-02 11:40:00", format="%Y-%m-%d %H:%M:%S") 
+  box_vert <- strptime("2007-02-02 09:30:00", format="%Y-%m-%d %H:%M:%S")
+  box_h <- strptime("2007-02-03 00:00:01", format="%Y-%m-%d %H:%M:%S")
   p<-ggplot(PowDat, aes(x=fullDate, y=Sub_metering_1)) +
     geom_line() + 
     geom_line(data=PowDat, aes(y=Sub_metering_2), color="red") +
@@ -22,13 +24,16 @@ if(file.exists("plot3.png")){
     xlab("") + 
     scale_x_datetime(breaks = date_breaks("1 day"), 
                      labels=date_format(format="%a")) +
-    geom_segment(aes(xend=x_end, y=33, yend=33, x=x_start), color="blue") + 
+    geom_segment(aes(x=x_start, xend=x_end-300, y=33, yend=33), color="blue") + 
     annotate("text", label="Sub_metering_3", y=33, x=x_end, hjust=0 ) + 
-    geom_segment(aes(xend=x_end, y=35, yend=35, x=x_start), color="red") + 
+    geom_segment(aes(x=x_start, xend=x_end-300, y=35, yend=35), color="red") + 
     annotate("text", label="Sub_metering_2", y=35, x=x_end, hjust=0 ) +
-    geom_segment(aes(xend=x_end, y=37, yend=37, x=x_start)) + 
-    annotate("text", label="Sub_metering_1", y=37, x=x_end, hjust=0 ) +
-    theme(legend.position="none")
+    geom_segment(aes(x=x_start, xend=x_end-300, y=37, yend=37)) + 
+    annotate("text", label="Sub_metering_1", y=37, x=x_end, hjust=0 ) + 
+    geom_segment(aes(x=box_vert,xend=box_vert, y=40, yend=31))+
+    geom_segment(aes(x=box_vert, xend=box_h, y=31, yend=31)) +
+    theme(legend.position="none")+
+    theme_bw()
 
   print(p)
   dev.copy(png, file = "plot3.png")
--- plot4.R
+++ plot4.R
@@ -1,11 +1,38 @@
 #Script plot4.R
 library(ggplot2)
 library(scales)
+source("multiplot.R")
+
+plot1 <-function(){ 
+  p1<-ggplot(PowDat, aes(x=fullDate, y=Global_active_power)) +
+    geom_line() + 
+    ylab("Global Active Power (kilowatts)") +
+    xlab("") + 
+    scale_x_datetime(breaks = date_breaks("1 day"), 
+                     labels=date_format(format="%a")) +
+    theme(legend.position="none") +
+    theme_bw()
+  return(p1)
+}
+
+plot2 <-function(){ 
+  p2<-ggplot(PowDat, aes(x=fullDate, y=Voltage)) +
+    geom_line() + 
+    ylab("Voltage") +
+    xlab("datetime") + 
+    scale_x_datetime(breaks = date_breaks("1 day"), 
+                     labels=date_format(format="%a")) +
+    theme(legend.position="none") +
+    theme_bw()
+  return(p2)
+}
 
 plot3 <-function(){  
   x_start <- strptime("2007-02-02 10:00:00", format="%Y-%m-%d %H:%M:%S")
   x_end <- strptime("2007-02-02 11:40:00", format="%Y-%m-%d %H:%M:%S") 
-  p3<-ggplot(PowDat, aes(x=fullDate, y=Sub_metering_1)) +
+  box_vert <- strptime("2007-02-02 09:30:00", format="%Y-%m-%d %H:%M:%S")
+  box_h <- strptime("2007-02-02 11:59:59", format="%Y-%m-%d %H:%M:%S")
+  p3<-ggplot(PowDat, aes(x=fullDate, y=Sub_metering_1), size=2) +
     geom_line() + 
     geom_line(data=PowDat, aes(y=Sub_metering_2), color="red") +
     geom_line(data=PowDat, aes(y=Sub_metering_3), color="blue") +
@@ -13,14 +40,29 @@ plot3 <-function(){
     xlab("") + 
     scale_x_datetime(breaks = date_breaks("1 day"), 
                      labels=date_format(format="%a")) +
-    geom_segment(aes(xend=x_end, y=33, yend=33, x=x_start), color="blue") + 
-    annotate("text", label="Sub_metering_3", y=33, x=x_end, hjust=0 ) + 
+    geom_segment(aes(xend=x_end, y=32, yend=32, x=x_start), color="blue") + 
+    annotate("text", label="Sub_metering_3", y=32, x=x_end, hjust=0, size=3 ) + 
     geom_segment(aes(xend=x_end, y=35, yend=35, x=x_start), color="red") + 
-    annotate("text", label="Sub_metering_2", y=35, x=x_end, hjust=0 ) +
-    geom_segment(aes(xend=x_end, y=37, yend=37, x=x_start)) + 
-    annotate("text", label="Sub_metering_1", y=37, x=x_end, hjust=0 ) +
-    theme(legend.position="none")
+    annotate("text", label="Sub_metering_2", y=35, x=x_end, hjust=0, size=3 ) +
+    geom_segment(aes(xend=x_end, y=38, yend=38, x=x_start)) + 
+    annotate("text", label="Sub_metering_1", y=38, x=x_end, hjust=0, size=3 ) +
+    geom_segment(aes(x=box_vert,xend=box_vert, y=40, yend=30), size=.25)+
+    geom_segment(aes(x=box_vert, xend=box_h, y=30, yend=30), size=.25) +
+    theme(legend.position="none") + 
+    theme_bw()
   return(p3)
+}
+
+plot4 <-function(){ 
+  p4<-ggplot(PowDat, aes(x=fullDate, y=Global_reactive_power)) +
+    geom_line() + 
+    ylab("Global_reactive_power") +
+    xlab("datetime") + 
+    scale_x_datetime(breaks = date_breaks("1 day"), 
+                     labels=date_format(format="%a")) +
+    theme(legend.position="none") +
+    theme_bw()
+  return(p4)
 }
 
 
@@ -34,9 +76,7 @@ if(file.exists("plot4.png")){
     source(paste0(getwd(), "/loadPowr.R"))
   } 
 
-
-  p3 <- plot3()
-  print(p3)
-  dev.copy(png, file = "plot4.png")
-  dev.off()
+  multiplot(plot1(), plot3(), plot2(), plot4(), cols=2)
+#  dev.copy(png, file = "plot4.png")
+#  dev.off()
 }
